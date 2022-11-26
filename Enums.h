55cbc9ceefe8b5a748239ea1b0db14cb70d9d6db
Peter Connell <peterconnellit@gmail.com>
2022-11-26 11:09:09 +0000

    enums for level bonuses

---
+++ Enums.h
@@ -0,0 +1,19 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum attackChoiceBonuses
+{
+	BONUSBLOCK = 10, BONUSATACK = 5
+};
+
+enum expBoundries
+{
+	LEVEL1 = 200, LEVEL2 = 300, LEVEL3 = 500, LEVEL4 = 800, LEVEL5 = 1200
+};
+
+enum healthPerLVL
+{
+	HEALTHBONUS = 100
+};
+
+#endif
