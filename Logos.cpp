127ccb794b846039ec277c461f04b1c80fae4858
Peter Connell <peterconnellit@gmail.com>
2022-11-27 22:24:19 +0000

    Created game logos

---
+++ Logos.cpp
@@ -0,0 +1,120 @@
+//Implementation of simple logos 
+#include"PrintFunctionality.h"
+#include"Logos.h"
+
+void printShield()
+{
+
+	//symbol before battle
+	printlnCenter("             ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶              ");
+	printlnCenter("      ¶¶¶¶¶¶¶¶¶           ¶¶¶¶¶¶¶¶¶       ");
+	printlnCenter("    ¶¶¶¶¶          ¶¶¶¶¶          ¶¶¶¶¶   ");
+	printlnCenter("    ¶¶           ¶¶¶¶¶¶¶¶¶            ¶¶  ");
+	printlnCenter("    ¶¶          ¶¶¶¶¶¶¶¶¶¶¶           ¶¶  ");
+	printlnCenter("    ¶¶              ¶¶¶¶¶¶            ¶¶  ");
+	printlnCenter("    ¶¶             ¶¶¶¶¶¶¶            ¶¶  ");
+	printlnCenter("    ¶¶      ¶¶   ¶¶¶¶¶¶¶¶¶            ¶¶  ");
+	printlnCenter("    ¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶ ¶     ¶¶  ");
+	printlnCenter("    ¶¶     ¶¶¶¶¶¶¶¶¶¶¶        ¶¶      ¶¶  ");
+	printlnCenter("    ¶¶    ¶¶¶¶¶¶¶¶¶¶¶¶¶       ¶¶¶     ¶¶  ");
+	printlnCenter("    ¶¶    ¶¶¶  ¶¶¶¶¶¶¶¶¶¶¶     ¶¶     ¶¶  ");
+	printlnCenter("    ¶¶      ¶¶    ¶¶¶¶¶¶¶¶¶    ¶¶     ¶¶  ");
+	printlnCenter("    ¶¶       ¶     ¶¶¶¶¶¶¶¶¶ ¶¶¶      ¶¶  ");
+	printlnCenter("    ¶¶             ¶¶¶¶¶¶¶¶¶¶¶        ¶¶  ");
+	printlnCenter("    ¶¶             ¶¶¶¶¶¶¶ ¶         ¶¶   ");
+	printlnCenter("     ¶¶       ¶¶   ¶¶¶¶¶¶¶          ¶¶    ");
+	printlnCenter("      ¶¶      ¶¶¶¶¶¶¶¶¶¶¶          ¶¶     ");
+	printlnCenter("       ¶¶             ¶¶¶         ¶¶      ");
+	printlnCenter("        ¶¶           ¶¶          ¶¶       ");
+	printlnCenter("         ¶¶         ¶¶          ¶¶        ");
+	printlnCenter("          ¶¶      ¶¶¶          ¶¶         ");
+	printlnCenter("           ¶¶¶               ¶¶           ");
+	printlnCenter("             ¶¶            ¶¶¶            ");
+	printlnCenter("               ¶¶¶       ¶¶¶              ");
+	printlnCenter("                  ¶¶¶  ¶¶¶                ");
+	printlnCenter("                     ¶¶                   ");
+
+}
+
+
+void printCup()
+{
+	//logo for victorry
+	printlnCenter("        ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶           ");
+	printlnCenter("        ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶           ");
+	printlnCenter("   ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ");
+	printlnCenter(" ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶   ");
+	printlnCenter("¶¶¶¶      ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶       ¶¶¶¶  ");
+	printlnCenter("¶¶¶       ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶        ¶¶¶  ");
+	printlnCenter("¶¶        ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶        ¶¶¶  ");
+	printlnCenter("¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶      ¶¶¶  ");
+	printlnCenter("¶¶¶    ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶    ¶¶¶¶  ");
+	printlnCenter(" ¶¶¶   ¶¶¶ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶¶    ¶¶¶   ");
+	printlnCenter(" ¶¶¶¶   ¶¶¶ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶  ¶¶¶¶    ");
+	printlnCenter("   ¶¶¶¶  ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶     ");
+	printlnCenter("    ¶¶¶¶¶¶¶¶ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶¶¶¶¶      ");
+	printlnCenter("      ¶¶¶¶¶¶  ¶¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶¶¶¶¶        ");
+	printlnCenter("               ¶¶¶¶¶¶¶¶¶¶¶¶                  ");
+	printlnCenter("                 ¶¶¶¶¶¶¶¶                    ");
+	printlnCenter("                   ¶¶¶¶                      ");
+	printlnCenter("                   ¶¶¶¶                      ");
+	printlnCenter("                   ¶¶¶¶                      ");
+	printlnCenter("                   ¶¶¶¶                      ");
+	printlnCenter("               ¶¶¶¶¶¶¶¶¶¶¶¶                  ");
+	printlnCenter("            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶               ");
+	printlnCenter("            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶               ");
+	printlnCenter("            ¶¶¶            ¶¶¶               ");
+	printlnCenter("            ¶¶¶            ¶¶¶               ");
+	printlnCenter("            ¶¶¶            ¶¶¶               ");
+	printlnCenter("            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶               ");
+	printlnCenter("            ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶               ");
+	printlnCenter("          ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶             ");
+	printlnCenter("         ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶            ");
+
+
+
+
+
+}
+
+
+void printSkull()
+{
+	//logo after you died
+	printlnCenter("                    ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶                    ");
+	printlnCenter("                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶                ");
+	printlnCenter("              ¶¶¶¶                       ¶¶¶¶              ");
+	printlnCenter("             ¶¶¶                             ¶¶            ");
+	printlnCenter("            ¶¶                                ¶¶           ");
+	printlnCenter("           ¶¶                                 ¶¶           ");
+	printlnCenter("          ¶¶                                   ¶¶          ");
+	printlnCenter("          ¶¶ ¶¶                             ¶¶ ¶¶          ");
+	printlnCenter("          ¶¶ ¶¶                             ¶¶  ¶          ");
+	printlnCenter("          ¶¶ ¶¶                             ¶¶  ¶          ");
+	printlnCenter("          ¶¶  ¶¶                            ¶¶ ¶¶          ");
+	printlnCenter("          ¶¶  ¶¶                           ¶¶  ¶¶          ");
+	printlnCenter("           ¶¶ ¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶ ¶¶           ");
+	printlnCenter("            ¶¶¶¶ ¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶           ");
+	printlnCenter("             ¶¶¶ ¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶             ");
+	printlnCenter("    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶   ");
+	printlnCenter("   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶  ");
+	printlnCenter("  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶  ");
+	printlnCenter(" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶ ");
+	printlnCenter("¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶        ¶¶");
+	printlnCenter("¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶");
+	printlnCenter("  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶ ");
+	printlnCenter("          ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶        ");
+	printlnCenter("              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶              ");
+	printlnCenter("                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶                 ");
+	printlnCenter("                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶              ");
+	printlnCenter("            ¶¶¶¶¶ ¶¶   ¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶ ¶¶¶¶¶            ");
+	printlnCenter("    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶    ");
+	printlnCenter("   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶   ");
+	printlnCenter("    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶     ¶¶¶    ");
+	printlnCenter("      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶      ");
+	printlnCenter("      ¶¶  ¶¶                                   ¶¶  ¶¶      ");
+	printlnCenter("       ¶¶¶¶                                     ¶¶¶¶       ");
+
+
+
+}
---
+++ Logos.h
@@ -0,0 +1,10 @@
+#ifndef LOGOS_H
+#define LOGOS_H
+
+//Logos interface to the user
+void printShield();
+void printCup();
+void printSkull();
+
+
+#endif // !LOGOS_H
